2df3b4716db344683dd0fc3b06e5c09f61b0ef92
beardykraken <vcasmirri@gmail.com>
2020-03-29 21:47:43 -0400

    Finished configuring burgers_controller.js (routes).

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,34 @@
+// Import Express and burger.js
+
+var express = require("express");
+
+var router = express.Routerp();
+
+var burgerFunctions = require("..models/burger.js");
+
+// Create routes and set up logic within routes
+
+router.get("/", function(req, res) {
+    burgerFunctions.selectAll(function(data){
+        var hbsObject = {
+            burgers: data
+        };
+        res.render("index", hbsObject);
+    });
+});
+
+router.post("/", function(req, res){
+    burgerFunctions.insertOne(req.body.burger_name), function(){
+        res.redirect("/");
+    };
+});
+
+router.put("/:id", function(req, res){
+    var condition = req.params.id;
+    burgerFunctions.updateOne(condition, function(){
+        res.redirect("/");
+    });
+});
+
+// Export the router
+module.exports = router;
